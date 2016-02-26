ffdf4cc03dbbe18b4dfbf9ab1b7228afbe0fee4f
Jasper Micah Davey <jasper.davey@gmail.com>
2016-02-26 00:06:28 -0500

    Initial Commit

---
+++ CS356Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS356Project
+//
+//  Created by Jasper Davey on 2/26/16.
+//  Copyright © 2016 Jasper Davey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
