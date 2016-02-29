cd06271fd82b69759a3abd87116e79f88ad28c08
Lucia Zapata <luciazapata@Lucias-Air.home>
2016-02-29 18:34:17 -0500

    here

--- CS356Project/server.c
+++ CS356Project/server.c
@@ -37,16 +37,17 @@ int main(int argc, char *argv[])
         if ( connfd )
         {
             printf( "Sending Least Cost Table\n" );
-            
-            if ( ( write(connfd, leastCost, sizeof( &leastCost ) / sizeof( int ) ) ) < 0 )
+            size_t arraySize = sizeof( leastCost ) / sizeof( leastCost[0] );
+	    printf( "%zu\n", arraySize );
+            if ( ( write(connfd, leastCost, arraySize ) ) < 0 )
             {
                 printf( "Error sending Least Cost Table\n" );
                 return 1;
             }
             
-            //printf( "Least Cost Table sent\n" );
         }
         
+	printf( "Least Cost Table sent\n" );
         close(connfd);
         sleep(1);
     }
